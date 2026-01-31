8ab633f933ecbbf21a88719a5b6e8d97717a17a4
vpacyn <vpacayucan@gmail.com>
2026-01-31 11:16:21 -0600

    changes

--- wax.js
+++ wax.js
@@ -4,15 +4,15 @@ const listaProductosWax = [
     // ==================================================================================
     
     // ========== PRODUCTOS WAX ACTIVOS ==========
-    { id: 101, categoria: 'wax', activo: true, orden: 1, agotado: false, nombre: "Ace Ultra 2G",  modelo: "2 Gramos", precio: 0, img: "img/wax/ace_ultra_2g.jpeg", sabores: [] },
-    { id: 102, categoria: 'wax', activo: true, orden: 2, agotado: false, nombre: "Ace Ultra X Packman 2G",  modelo: "2 Gramos", precio: 0, img: "img/wax/ace_ultra_x_packman_2g.jpeg", sabores: [] },
-    { id: 103, categoria: 'wax', activo: true, orden: 3, agotado: false, nombre: "Cookies 2G",  modelo: "2 Gramos", precio: 0, img: "img/wax/cookies_2g.jpeg", sabores: [] },
-    { id: 104, categoria: 'wax', activo: true, orden: 4, agotado: false, nombre: "Luigi 2G",  modelo: "2 Gramos", precio: 0, img: "img/wax/luigi_2g.jpeg", sabores: [] },
-    { id: 105, categoria: 'wax', activo: true, orden: 5, agotado: false, nombre: "Packman 2G",  modelo: "2 Gramos", precio: 0, img: "img/wax/packman_2g.jpeg", sabores: [] },
-    { id: 106, categoria: 'wax', activo: true, orden: 6, agotado: false, nombre: "Pack woods 2G",  modelo: "2 Gramos", precio: 0, img: "img/wax/packwoods_2g.jpeg", sabores: [] },
-    { id: 107, categoria: 'wax', activo: true, orden: 7, agotado: false, nombre: "Big Chief",  modelo: "2 Gramos", precio: 0, img: "img/wax/bigchief.webp", sabores: [] },
-    { id: 108, categoria: 'wax', activo: true, orden: 8, agotado: false, nombre: "Muhameds 3.5g",  modelo: "3 Gramos", precio: 0, img: "img/wax/muhameds.webp", sabores: [] },
-    { id: 109, categoria: 'wax', activo: true, orden: 9, agotado: false, nombre: "Rick and Morty 2G",  modelo: "2 Gramos", precio: 0, img: "img/wax/rick_and_morty.webp", sabores: [] },
+    { id: 101, categoria: 'wax', activo: true, orden: 1, agotado: false, nombre: "Ace Ultra 2G",  modelo: "2 Gramos", precio: "COTIZAR", img: "img/wax/ace_ultra_2g.jpeg", sabores: [] },
+    { id: 102, categoria: 'wax', activo: true, orden: 2, agotado: false, nombre: "Ace Ultra X Packman 2G",  modelo: "2 Gramos", precio: "COTIZAR", img: "img/wax/ace_ultra_x_packman_2g.jpeg", sabores: [] },
+    { id: 103, categoria: 'wax', activo: true, orden: 3, agotado: false, nombre: "Cookies 2G",  modelo: "2 Gramos", precio: "COTIZAR", img: "img/wax/cookies_2g.jpeg", sabores: [] },
+    { id: 104, categoria: 'wax', activo: true, orden: 4, agotado: false, nombre: "Luigi 2G",  modelo: "2 Gramos", precio: "COTIZAR", img: "img/wax/luigi_2g.jpeg", sabores: [] },
+    { id: 105, categoria: 'wax', activo: true, orden: 5, agotado: false, nombre: "Packman 2G",  modelo: "2 Gramos", precio: "COTIZAR", img: "img/wax/packman_2g.jpeg", sabores: [] },
+    { id: 106, categoria: 'wax', activo: true, orden: 6, agotado: false, nombre: "Pack woods 2G",  modelo: "2 Gramos", precio: "COTIZAR", img: "img/wax/packwoods_2g.jpeg", sabores: [] },
+    { id: 107, categoria: 'wax', activo: true, orden: 7, agotado: false, nombre: "Big Chief",  modelo: "2 Gramos", precio: "COTIZAR", img: "img/wax/bigchief.webp", sabores: [] },
+    { id: 108, categoria: 'wax', activo: true, orden: 8, agotado: false, nombre: "Muhameds 3.5g",  modelo: "3 Gramos", precio: "COTIZAR", img: "img/wax/muhameds.webp", sabores: [] },
+    { id: 109, categoria: 'wax', activo: true, orden: 9, agotado: false, nombre: "Rick and Morty 2G",  modelo: "2 Gramos", precio: "COTIZAR", img: "img/wax/rick_and_morty.webp", sabores: [] },
    // ========== PRODUCTOS WAX INACTIVOS ==========
     { id: 110, categoria: 'wax', activo: false, orden: 10, agotado: false, nombre: "Wax Badder 1G",  modelo: "1 Gramo", precio: 175, img: "img/wax/wax_10.webp", sabores: [] },
     { id: 111, categoria: 'wax', activo: false, orden: 11, agotado: false, nombre: "Wax Pull & Snap 1G",  modelo: "1 Gramo", precio: 155, img: "img/wax/wax_11.webp", sabores: [] },
