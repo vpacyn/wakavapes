65b8153928899b03adac789a105960a073cf681f
Purgan05 <ikerbaba05@gmail.com>
2026-03-08 13:17:32 -0600

    agotado

--- vapes.js
+++ vapes.js
@@ -198,7 +198,7 @@ let listaProductos = [
         { nombre: "Sour Apple", disponible: true },
         { nombre: "Clear", disponible: true }
     ] },
-    { id: 12, categoria: 'vapes', activo: true, orden: 12, agotado: false, nombre: "Halo SYNIX 30K",     modelo: "30000 Puffs", precio: 105, img: "img/vapes/halo_30k.webp",       sabores: [
+    { id: 12, categoria: 'vapes', activo: true, orden: 12, agotado: true, nombre: "Halo SYNIX 30K",     modelo: "30000 Puffs", precio: 105, img: "img/vapes/halo_30k.webp",       sabores: [
         { nombre: "Strawberry Kiwi", disponible: true },
         { nombre: "Watermelon Ice", disponible: true },
         { nombre: "Mexican Mango", disponible: true },
